c9095291f62ff493deef02fcb169163ac9696aa5
Xavier MD <37403873+XMousset@users.noreply.github.com>
2026-07-15 02:05:24 +0200

    improve clarity and add checks after csv creation

--- main.py
+++ main.py
@@ -12,10 +12,9 @@ def get_spotify_id():
     load_dotenv()
     client_id = os.getenv("CLIENT_ID")
     client_secret = os.getenv("CLIENT_SECRET")
-    assert client_id is not None, "CLIENT_ID must be set in the .env file"
-    assert (
-        client_secret is not None
-    ), "CLIENT_SECRET must be set in the .env file"
+    msg = "must be set in the .env file"
+    assert client_id is not None, f"CLIENT_ID {msg}"
+    assert client_secret is not None, f"CLIENT_SECRET {msg}"
     spotify_id = {
         "CLIENT_ID": client_id,
         "CLIENT_SECRET": client_secret,
@@ -23,44 +22,48 @@ def get_spotify_id():
     return spotify_id
 
 
-def get_deck_from_playlist(
-    playlist_id: str, output_path: str | Path | None = None
+def create_csv_from_playlist(
+    playlist_id: str,
+    output_path: str | Path | None = None,
 ):
     """Fetch playlist data from Spotify and save dataset to CSV.
     Returns a HitsterDeck object with the playlist data."""
+    if isinstance(output_path, str):
+        if not output_path.endswith(".csv"):
+            output_path += ".csv"
+        output_path = Path(output_path)
+
     sp_api = SpotifyAPI(**get_spotify_id())
     name = sp_api.get_playlist_name(playlist_id)
     df = sp_api.get_playlist_info(playlist_id)
 
     deck = HitsterDeck(name=name)
     deck.data = df
-    if isinstance(output_path, str):
-        output_path = Path(output_path)
-
-    deck.save_to_csv(file_path=output_path)
+    deck.save_to_csv(output_path)
 
     return deck
 
 
-def get_deck_from_csv(file_path: str | Path):
+def create_deck_from_csv(file_path: str | Path):
     """Load playlist data from a CSV file.
     Returns a HitsterDeck object with the playlist data."""
     if isinstance(file_path, str):
+        if not file_path.endswith(".csv"):
+            file_path += ".csv"
         file_path = Path(file_path)
 
-    name = file_path.stem
-    deck = HitsterDeck(name=name)
-    deck.load_from_csv(file_path=file_path)
-
+    deck = HitsterDeck(file_path.stem)
+    deck.load_from_csv(file_path)
     return deck
 
 
 if __name__ == "__main__":
-    playlist = "spotify_playlist_URI_goes_here"
+    PLAYLIST = "spotify_playlist_URI_goes_here"
+    CSV_FILE = "output_data.csv"
 
-    deck = get_deck_from_playlist(playlist, "your_playlist.csv")
-    # deck = get_deck_from_csv("your_playlist.csv")
+    deck = create_csv_from_playlist(PLAYLIST, CSV_FILE)
+    deck = create_deck_from_csv(CSV_FILE)
 
-    deck.analyse_dataframe()
-
+    # deck.check_data_integrity()  # check for missing values and duplicates
+    # deck.analyse_dataframe()  # histogram of the number of songs per year
     deck.create_hitster()
